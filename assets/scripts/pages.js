7b51791350b21c85a360db7b98d922e14252753a
Umair Akram <hello@umairakram.tech>
2024-07-28 01:33:44 +0500

    portfolio uploaded

---
+++ assets/scripts/pages.js
@@ -0,0 +1,29 @@
+let navigation = document.querySelectorAll(".navigation");
+let mainData = document.querySelector(".data");
+
+for(let i = 0; i < 4; i++){
+    if(window.location.href === navigation[i].href){
+        navigation[i].style.color = "#183D3D";
+    }
+}
+let hamburger = document.querySelector(".fa-bars");
+let offCanvas = document.querySelector(".off-canvas");
+let cross = document.querySelector(".fa-xmark");
+let showOffCanvas = () => {
+    offCanvas.style.transition = "width 1s ease-out"
+    offCanvas.style.width = "200px"
+    offCanvas.style.right = "10px"
+    offCanvas.style.padding = "20px 20px";
+}
+let hideOffCanvas = () => {
+    offCanvas.style.transition = "width 1s ease-in"
+    offCanvas.style.width = "0"
+    offCanvas.style.right = "0"
+    setTimeout(() => {
+        offCanvas.style.padding = "20px 0";
+
+    }, "1000");
+
+}
+hamburger.addEventListener("click", showOffCanvas);
+cross.addEventListener("click", hideOffCanvas);
