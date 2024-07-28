117cd14c8859f26388709a7f1f331e7fe7eb99d3
Umair Akram <hello@umairakram.tech>
2024-07-28 05:16:13 +0500

    Profile view on mobile and laptop

--- assets/scripts/pages.js
+++ assets/scripts/pages.js
@@ -1,24 +1,28 @@
 let navigation = document.querySelectorAll(".navigation");
 let mainData = document.querySelector(".data");
+let hamburger = document.querySelector(".fa-bars");
+let offCanvas = document.querySelector(".off-canvas");
+let cross = document.querySelector(".fa-xmark");
+let sideBar = document.querySelector(".sidebar");
+let userIcon = document.querySelector(".fa-user");
+let moveBack = document.querySelector(".fa-arrow-left");
 
 for(let i = 0; i < 8; i++){
     if(window.location.href === navigation[i].href){
         navigation[i].style.color = "#183D3D";
     }
 }
-let hamburger = document.querySelector(".fa-bars");
-let offCanvas = document.querySelector(".off-canvas");
-let cross = document.querySelector(".fa-xmark");
+
 let showOffCanvas = () => {
-    offCanvas.style.transition = "width 1s ease-out"
-    offCanvas.style.width = "200px"
-    offCanvas.style.right = "10px"
+    offCanvas.style.transition = "width 1s ease-out";
+    offCanvas.style.width = "200px";
+    offCanvas.style.right = "10px";
     offCanvas.style.padding = "20px 20px";
 }
 let hideOffCanvas = () => {
-    offCanvas.style.transition = "width 1s ease-in"
-    offCanvas.style.width = "0"
-    offCanvas.style.right = "0"
+    offCanvas.style.transition = "width 1s ease-in";
+    offCanvas.style.width = "0";
+    offCanvas.style.right = "0";
     setTimeout(() => {
         offCanvas.style.padding = "20px 0";
 
@@ -27,3 +31,18 @@ let hideOffCanvas = () => {
 }
 hamburger.addEventListener("click", showOffCanvas);
 cross.addEventListener("click", hideOffCanvas);
+
+let showProfile = () => {
+    sideBar.style.transition = "width 1s ease-out";
+    sideBar.style.width = "100%";
+    sideBar.style.visibility = "visible";
+}
+let hideProfile = () => {
+    sideBar.style.transition = "width 1s ease-in";
+    sideBar.style.width = "0";
+    setTimeout(() => {
+        sideBar.style.visibility = "hidden";
+    }, "1000");
+}
+userIcon.addEventListener("click", showProfile);
+moveBack.addEventListener("click", hideProfile);
